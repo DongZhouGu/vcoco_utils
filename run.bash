e7a4a9921e662bb736a4a382cc391d4cb2d20279
DongZhouGuu <gdz678@163.com>
2021-01-14 22:39:27 +0800

    UPDATE

--- run.bash
+++ run.bash
@@ -1,5 +1,5 @@
 git remote add origin git@github.com:DongZhouGu/vcoco_utils.git 
 git add .
 git commit -m 'UPDATE'
-git pull –rebase origin master
-git push -u origin master
+git pull –rebase origin main
+git push -u origin main
